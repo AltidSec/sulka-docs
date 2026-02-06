f69c35140a5efff9de5cc70fde350d4e9392cc9d
ejaaskel <esa.jaaskela@suomi24.fi>
2026-02-06 18:00:14 +0000

    Bump version to 0.4.0

--- source/conf.py
+++ source/conf.py
@@ -6,8 +6,8 @@ project = 'Sulka Documentation'
 copyright = '2025, AltidSec'
 author = 'AltidSec'
 
-release = '0.3.0'
-version = '0.3.0'
+release = '0.4.0'
+version = '0.4.0'
 
 # -- General configuration
 
